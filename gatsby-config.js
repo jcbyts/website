e3aac35ce2bce17796455fe6994de4d1686bfe11
jcbyts <jacoby8s@gmail.com>
2019-09-19 22:08:59 -0400

    add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,7 @@ module.exports = {
     },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
