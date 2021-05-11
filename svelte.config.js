42a0b86734435e0a2dadc166a5aa47a965e4b4b6
Amelia Wattenbeger <wattenberger@github.com>
2021-05-11 12:33:28 -0400

    update mdsvex import

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import { mdsvex } from "mdsvex"
-import mdsvexConfig from "./mdsvex.config.cjs"
-import netlifyAdapter from "@sveltejs/adapter-netlify"
+import mdsvex from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.cjs";
+import netlifyAdapter from "@sveltejs/adapter-netlify";
 /** @type {import('@sveltejs/kit').Config} */
 
 export default {
-  preprocess: [mdsvex(mdsvexConfig)],
+  preprocess: [mdsvex.mdsvex(mdsvexConfig)],
   extensions: [".svelte", ...mdsvexConfig.extensions],
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
