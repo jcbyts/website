6647cd817d2f51c7cf13df80c1fcbb5119323c74
Amelia Wattenbeger <wattenberger@github.com>
2022-01-09 17:40:00 -0500

    add research timeline structure

---
+++ src/components/ResearchTimeline.svelte
@@ -0,0 +1,32 @@
+<script>
+  const timeline = {
+    stimuli: [
+      {
+        name: "",
+        dates: [1960],
+        description: "",
+      },
+    ],
+    species: [
+      {
+        name: "",
+        dates: [1960, 2000],
+        description: "",
+      },
+    ],
+    technology: [
+      {
+        name: "",
+        dates: [1960],
+        description: "",
+      },
+    ],
+    behavior: [
+      {
+        name: "",
+        dates: [1960],
+        description: "",
+      },
+    ],
+  };
+</script>
