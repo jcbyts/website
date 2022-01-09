e4b17d2f4d4c598d6793b8455fb9a9effbc87e13
Amelia Wattenbeger <wattenberger@github.com>
2022-01-09 10:38:21 -0500

    fix "page" error on blog

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-  export async function load({ page, fetch, session, context }) {
+  export async function load({ fetch }) {
     const url = "/blog.json";
 
     const res = await fetch(url);
